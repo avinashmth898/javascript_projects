e2625ec605984ec38cf0106bb68dd2f5b1015917
avinashmth898 <47936739+avinashmth898@users.noreply.github.com>
2024-01-16 15:51:55 +0000

    clock_project done

--- p2-form/bmi.js
+++ p2-form/bmi.js
@@ -8,6 +8,27 @@ form.addEventListener('submit',function(e){
     const results = document.querySelector('#results');
     const bmi= (weight/((height*height)/10000)).toFixed(2); 
     console.log(bmi)
-    results.innerHTML= `<br><span>The BMI is: ${bmi}</span>`
+    if(bmi<=18.5){results.innerHTML=`<br><span>The BMI is: ${bmi}</span><p> 
+    <p>Underweight</p>
+    BMI Categories:</p>
+    <p> Underweight = <18.5</p>
+        <p>  Normal weight = 18.5–24.9</p>
+            <p>  Overweight = 25–29.9</p>
+<p> Obesity = BMI of 30 or greater</p>`}
+if(bmi>18.5 && bmi<=24.9){results.innerHTML=`<br><span>The BMI is: ${bmi}</span><p> 
+    <p>Normal weight</p>
+    BMI Categories:</p>
+    <p> Underweight = <18.5</p>
+        <p>  Normal weight = 18.5–24.9</p>
+            <p>  Overweight = 25–29.9</p>
+<p> Obesity = BMI of 30 or greater</p>`}
+if(bmi>24.9){results.innerHTML=`<br><span>The BMI is: ${bmi}</span><p> 
+    <p>Overweight</p>
+    BMI Categories:</p>
+    <p> Underweight = <18.5</p>
+        <p>  Normal weight = 18.5–24.9</p>
+            <p>  Overweight = 25–29.9</p>
+<p> Obesity = BMI of 30 or greater</p>`}
+    
     
 });
---
+++ p3-digital_clock/time.js
@@ -0,0 +1,11 @@
+const clock=document.getElementById('clock')
+// document.querySelector('#clock')
+
+// console.log(date.toLocaleDateString())
+
+setInterval(function(){
+    let date=new Date()
+    time=date.toLocaleTimeString()
+    clock.innerHTML=`<span>${time}</span>`
+    console.log(date.toLocaleTimeString())
+},1000)
