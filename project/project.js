16cfeff53570db0f0598bf2cf5df41ca1f63373b
avinashmth898 <47936739+avinashmth898@users.noreply.github.com>
2024-01-29 12:21:10 +0000

    updated

---
+++ project/project.js
@@ -0,0 +1,21 @@
+document.getElementById('lake').onclick =function(){
+    alert("Lake Clicked")
+}
+// To select individual tag(jatayu in this case)
+// document.getElementById('jatayu').addEventListener('click',function(e) {
+//     alert('Clicked on Jatayu Earth Centre')
+//     console.log(e)
+//     console.log(e.timeStamp)
+// }, false)
+//types, timestamp, defaultPrevented, 
+// target, toElement, srcElement, currentTarget
+// clientX, clientY, screenX, screenY
+// altkey, ctrlkey, shiftkey, keyCode
+
+//event propagation
+// To select the images tag (the parent tag)
+document.getElementById('images').addEventListener('click',function(e) {
+    alert('Clicked on images Earth Centre')
+    console.log(e)
+    console.log(e.timeStamp)
+}, false)
